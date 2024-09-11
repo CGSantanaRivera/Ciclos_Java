f9e973ed31d92cb3055ffd9a0e3fb4346f741e30
Carlos Santana <carlosgabriel46@gmail.com>
2024-09-11 15:37:45 -0600

    Se modifica .gitignore y se crean los ejercicios Numeros, Triangulo y TrianguloReversa

---
+++ src/Triangulo.java
@@ -0,0 +1,12 @@
+public class Triangulo {
+	public static void main(String[] args) {
+		System.out.println("===========For Triangulo==========");
+		for(int t1=1; t1<=5; t1++) {
+			
+			for(int t2=1; t2<=t1; t2++) {
+			System.out.print(" * ");
+		}//for cont
+		System.out.println();
+		}
+	}	
+}
---
+++ src/TrianguloReversa.java
@@ -0,0 +1,12 @@
+public class TrianguloReversa {
+	public static void main(String[] args) {
+		System.out.println("===========For TrianguloReversa==========");
+		for(int t1=5; t1>=1; t1--) {
+			
+			for(int t2=1; t2<=t1; t2++) {
+			System.out.print(" * ");
+		}//for cont
+		System.out.println();
+		}
+	}	
+}
